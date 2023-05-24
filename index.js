3aec7040cc78e4d9921f4e0540ed985ba48f6b8f
olivia <yli514@my.bcit.ca>
2023-05-23 17:30:21 -0700

    Step 9 + Step 10: Corner case 2 + 3

--- index.js
+++ index.js
@@ -2,18 +2,19 @@
 
 
 const setup = () => {
-  let firstCard = undefined
-  let secondCard = undefined
+  let firstCard = null;
+  let secondCard = null;
+  let comparing = false;
   $(".card").on(("click"), function () {
-    if ($(this).hasClass("flip")) {
+    if ($(this).hasClass("flip") || comparing) {
       return;
     }
     $(this).toggleClass("flip");
 
     if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
+      firstCard = $(this).find(".front_face")[0];
     else {
-      secondCard = $(this).find(".front_face")[0]
+      secondCard = $(this).find(".front_face")[0];
       console.log(firstCard, secondCard);
       if (
         firstCard.src
@@ -21,14 +22,21 @@ const setup = () => {
         secondCard.src
       ) {
         console.log("match")
-        $(`#${firstCard.id}`).parent().off("click")
-        $(`#${secondCard.id}`).parent().off("click")
+        $(`#${firstCard.id}`).parent().off("click");
+        $(`#${secondCard.id}`).parent().off("click");
+        firstCard = null;
+        secondCard = null;
       } else {
         console.log("no match")
+        comparing = true;
         setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
+          $(`#${firstCard.id}`).parent().toggleClass("flip");
+          $(`#${secondCard.id}`).parent().toggleClass("flip");
+          firstCard = null;
+          secondCard = null;
+          comparing = false;
         }, 1000)
+
       }
     }
   });
